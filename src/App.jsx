fa1f93b9c7ea52ecc1e6d3ced731359840751a69
Gakiri001 <gakirijulius@gmail.com>
2025-07-06 09:58:20 +0300

    installing tailwing and shadCN

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-
+import "./assets/global.css"
+import "./App.css"
+import { Button } from "./components/ui/button"
 
 function App() {
 
   return (
     <>
-    <h2>Oyah</h2>
+    <h2 className="bg-red-300">Oyah</h2>
+    <Button>Gathee</Button>
     </>
   )
 }
